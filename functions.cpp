c6fbb5816e60c3d47bf28260ad4517280aa8a16f
mdcryptonfts <105838382+mdcryptonfts@users.noreply.github.com>
2023-07-31 08:53:48 -0400

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -5,6 +5,16 @@ void royaltyshare::announce_dao_deposit(const name& dao_name, const symbol& toke
 	return;
 }
 
+void royaltyshare::check_token_exists(const symbol& token_symbol, const name& contract){
+	const uint64_t raw_token_symbol = token_symbol.code().raw();
+	const uint128_t symbol_contract_combo = mix64to128(raw_token_symbol, contract.value);
+	stat_table stat(contract, token_symbol.code().raw());
+	auto stat_itr = stat.find(token_symbol.code().raw());
+	check(stat_itr != stat.end(), "That token does not exist on that contract");
+	check(stat_itr->supply.symbol == token_symbol, "Symbol mismatch. You probably put the wrong amount of decimals in the precision field");
+	return;
+}
+
 void royaltyshare::check_username(const name& user){
 	auto itr = config_t.require_find(0, "Could not locate config");
 	check(std::find(itr->authorized_accounts.begin(), itr->authorized_accounts.end(), user) != itr->authorized_accounts.end(), "You aren't authorized to perform this action");	
