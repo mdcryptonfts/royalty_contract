b1f6efdda106c0e3bb902739e5479e2060f843c2
mdcryptonfts <105838382+mdcryptonfts@users.noreply.github.com>
2023-07-31 08:50:22 -0400

    Update structs.hpp

--- structs.hpp
+++ structs.hpp
@@ -7,21 +7,45 @@ struct COLLECTION_AND_SCHEMA {
 
 struct STAKEABLE_COLLECTION {
 	eosio::name  		collection_name;
-	uint8_t  			is_authorized; 
+	uint8_t  		is_authorized; 
 };
 
 struct RECEIVING_DAO {
 	eosio::name  		dao_name;
-	uint64_t  			parts; 
+	uint64_t  		parts; 
 };
 
 struct RECEIVING_FARM {
+	eosio::name  		contract_name;
 	eosio::name  		farm_name;
-	uint64_t  			parts; 
+	uint64_t  		parts; 
 };
 
 struct RECEIVING_WALLET {
 	eosio::name  		wax_address;
-	uint64_t  			parts; 
+	uint64_t  		parts; 
 	std::string  		memo;
+};
+
+struct REWARD_POOL {
+	eosio::asset	total_funds;
+	eosio::name  	contract;
+	eosio::asset  	total_hourly_reward;
+};
+
+struct FARM_PROFILE {
+	std::string 		avatar;
+	std::string  		cover_image;
+	std::string  		description; 
+};
+
+struct SOCIAL_LINKS {
+	std::string 		website;
+	std::string  		telegram;
+	std::string  		discord; 
+	std::string  		twitter; 
+	std::string  		medium; 
+	std::string  		youtube;
+	std::string  		atomichub;
+	std::string  		waxdao;
 };
