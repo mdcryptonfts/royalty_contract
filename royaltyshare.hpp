3a694c68fa13865012d1389d3acdd6522e032973
mdcryptonfts <105838382+mdcryptonfts@users.noreply.github.com>
2023-07-31 08:48:09 -0400

    Update royaltyshare.hpp

--- royaltyshare.hpp
+++ royaltyshare.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #define CONTRACT_NAME "royaltyshare"
+#define mix64to128(a, b) (uint128_t(a) << 64 | uint128_t(b))
 
 #include <eosio/eosio.hpp>
 #include <eosio/print.hpp>
@@ -18,8 +19,6 @@
 #include <limits>
 #include "../tables/atomicdata.hpp"
 
-#define mix64to128(a, b) (uint128_t(a) << 64 | uint128_t(b))
-
 using namespace eosio;
 
 
@@ -30,18 +29,29 @@ CONTRACT royaltyshare : public contract {
 
 		//Main Actions
 		ACTION addauthacct(const name& account_to_add);
+		ACTION addrule(const name& user, const symbol& token_symbol, const name& contract);
 		ACTION allowalltokn(const name& user, const uint8_t& is_allowed);
 		ACTION claimroyalty();
 		ACTION initconfig();
-		ACTION insertdao(const name& user, const name& dao_name, const uint64_t& parts);
-		ACTION insertfarm(const name& user, const name& farm_name, const uint64_t& parts);
-		ACTION insertwallet(const name& user, const name& wax_address, const uint64_t& parts,
-			const std::string& memo);
 		ACTION removedao(const name& user, const name& dao_name);
 		ACTION removefarm(const name& user, const name& farm_name);
+		ACTION removerule(const name& user, const symbol& token_symbol, const name& contract);
 		ACTION removewallet(const name& user, const name& wax_address);
 		ACTION rmvauthacct(const name& account_to_remove);
+		ACTION ruledaormv(const name& user, const name& dao_name, const symbol& token_symbol, const name& contract);
+		ACTION ruledaoupdt(const name& user, const name& dao_name, const uint64_t& parts, 
+			const symbol& token_symbol, const name& contract);
+		ACTION rulefarmrmv(const name& user, const name& farm_name, const symbol& token_symbol, const name& contract);
+		ACTION rulefarmupdt(const name& user, const name& farm_contract, const name& farm_name, const uint64_t& parts,
+			const symbol& token_symbol, const name& token_contract);
+		ACTION rulewlltrmv(const name& user, const name& wax_address, const symbol& token_symbol, const name& contract);
+		ACTION rulewlltupdt(const name& user, const name& wax_address, const uint64_t& parts,
+			const std::string& memo, const symbol& token_symbol, const name& contract);
 		ACTION updatehours(const name& your_wallet, const uint64_t& hours_between_claims);
+		ACTION upsertdao(const name& user, const name& dao_name, const uint64_t& parts);
+		ACTION upsertfarm(const name& user, const name& farm_contract, const name& farm_name, const uint64_t& parts);
+		ACTION upsertwallet(const name& user, const name& wax_address, const uint64_t& parts,
+			const std::string& memo);		
 
 		//Notifications
 		[[eosio::on_notify("eosio.token::transfer")]] void receive_wax_transfer(const name& from, const name& to, const eosio::asset& quantity, const std::string& memo);
@@ -54,17 +64,17 @@ CONTRACT royaltyshare : public contract {
 		balances_t balances = balances_t(ATOMICMARKET_CONTRACT, ATOMICMARKET_CONTRACT.value);
 		configtable config_t = configtable(get_self(), get_self().value);
 		daotable daos_t = daotable(DAO_CONTRACT, DAO_CONTRACT.value);
-		farms_table farms_t = farms_table(FARM_CONTRACT, FARM_CONTRACT.value);
+		farms_table farms_t = farms_table(FARM_CONTRACT_V1, FARM_CONTRACT_V1.value);
+		v2_farms_t farms_t_v2 = v2_farms_t(FARM_CONTRACT_V2, FARM_CONTRACT_V2.value);
+		rules_table rules_t = rules_table(get_self(), get_self().value);
 
 
 		//Functions
 		void announce_dao_deposit(const name& dao_name, const symbol& token_symbol, const name& token_contract);
-		void check_username(const name& user);;
+		void check_token_exists(const symbol& token_symbol, const name& contract);
+		void check_username(const name& user);
 		void claim_from_atomicmarket(const eosio::asset& balance);
 		uint64_t now();
 		void transfer_tokens(const name& user, const asset& amount_to_send, const name& contract, const std::string& memo);
 
 };
-
-
-
