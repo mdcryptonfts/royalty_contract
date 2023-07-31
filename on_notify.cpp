463a1745065f7000a6a73eb37ec1996a1a99ddd9
mdcryptonfts <105838382+mdcryptonfts@users.noreply.github.com>
2023-07-31 08:49:42 -0400

    Update on_notify.cpp

--- on_notify.cpp
+++ on_notify.cpp
@@ -6,7 +6,7 @@ void royaltyshare::receive_wax_transfer(const name& from, const name& to, const 
         return;
     }
 
-	const name tkcontract = get_first_receiver();
+    const name tkcontract = get_first_receiver();
 
     check(quantity.amount > 0 && quantity.amount <= MAX_ASSET_AMOUNT, "Invalid quantity."); 
 
@@ -56,19 +56,26 @@ void royaltyshare::receive_wax_transfer(const name& from, const name& to, const 
 
     for(auto f : linked_farms){
         uint64_t amount_to_send = amount_per_part * f.parts;
-        transfer_tokens(FARM_CONTRACT, asset(amount_to_send, quantity.symbol), tkcontract, FARM_DEPOSIT_MEMO(f.farm_name));
+        if(f.contract_name == FARM_CONTRACT_V1){
+            transfer_tokens(FARM_CONTRACT_V1, asset(amount_to_send, quantity.symbol), tkcontract, FARM_DEPOSIT_MEMO(f.farm_name));
+        }
+        else if(f.contract_name == FARM_CONTRACT_V2){
+            transfer_tokens(FARM_CONTRACT_V2, asset(amount_to_send, quantity.symbol), tkcontract, FARM_DEPOSIT_MEMO(f.farm_name));
+        }        
     }  
 
     for(auto w : linked_wallets){
         uint64_t amount_to_send = amount_per_part * w.parts;
-        transfer_tokens(w.wax_address, asset(amount_to_send, quantity.symbol), tkcontract, w.memo);
+        if(w.wax_address != get_self()){
+            transfer_tokens(w.wax_address, asset(amount_to_send, quantity.symbol), tkcontract, w.memo);
+        }
     }        
 
 } 
 
 /** 
  *  receive_nft_transfer
- *  this is required to that the catchall *::transfer does not throw an error when
+ *  this is required so that the catchall *::transfer does not throw an error when
  *  nfts are sent to this contract
  */
 
@@ -86,8 +93,9 @@ void royaltyshare::receive_any_transfer(const name& from, const name& to, const 
 
     check(quantity.amount > 0 && quantity.amount <= MAX_ASSET_AMOUNT, "Invalid quantity."); 
 
-    /** We aren't accounting for farm shares here, because we only deal with farms
-     *  where WAX is the reward token. That is handled in receive_wax_transfer.
+    /** We aren't accounting for farm shares here, UNLESS there is a rule set for this token
+     *  because we only deal with farms where WAX is the reward token. 
+     *  That is handled in receive_wax_transfer.
      */
 
     auto c_it = config_t.require_find(0, ERR_CONFIG_NOT_FOUND);
@@ -100,8 +108,24 @@ void royaltyshare::receive_any_transfer(const name& from, const name& to, const 
 
     uint64_t total_parts = 0;
 
-    std::vector<RECEIVING_DAO> linked_daos = c_it->linked_daos;
-    std::vector<RECEIVING_WALLET> linked_wallets = c_it->linked_wallets;
+    auto rules_secondary = rules_t.get_index<"symcontract"_n>();
+    const uint128_t symbol_contract_combo = mix64to128(quantity.symbol.code().raw(), tkcontract.value);
+    auto rules_itr = rules_secondary.find(symbol_contract_combo);
+
+    std::vector<RECEIVING_DAO> linked_daos {};
+    std::vector<RECEIVING_WALLET> linked_wallets {};
+    std::vector<RECEIVING_FARM> linked_farms {};
+
+    if(rules_itr != rules_secondary.end()){
+        linked_daos = rules_itr->linked_daos;
+        linked_wallets = rules_itr->linked_wallets;
+        linked_farms = rules_itr->linked_farms;
+    }
+
+    else{
+        linked_daos = c_it->linked_daos;
+        linked_wallets = c_it->linked_wallets;    
+    }
 
     if(linked_daos.size() > 0){
         for(auto d : linked_daos){
@@ -113,7 +137,13 @@ void royaltyshare::receive_any_transfer(const name& from, const name& to, const 
         for(auto w : linked_wallets){
             total_parts += w.parts;
         }
-    }       
+    }  
+
+    if(linked_farms.size() > 0){
+        for(auto f : linked_farms){
+            total_parts += f.parts;
+        }
+    }           
 
     if(total_parts == 0){
         return; //nothing to do, since we have no instructions on where to send funds
@@ -135,7 +165,19 @@ void royaltyshare::receive_any_transfer(const name& from, const name& to, const 
 
     for(auto w : linked_wallets){
         uint64_t amount_to_send = amount_per_part * w.parts;
-        transfer_tokens(w.wax_address, asset(amount_to_send, quantity.symbol), tkcontract, w.memo);
-    }      
+        if(w.wax_address != get_self()){
+            transfer_tokens(w.wax_address, asset(amount_to_send, quantity.symbol), tkcontract, w.memo);
+        }
+    } 
+
+    for(auto f : linked_farms){
+        uint64_t amount_to_send = amount_per_part * f.parts;
+        if(f.contract_name == FARM_CONTRACT_V1){
+            transfer_tokens(FARM_CONTRACT_V1, asset(amount_to_send, quantity.symbol), tkcontract, FARM_DEPOSIT_MEMO(f.farm_name));
+        }
+        else if(f.contract_name == FARM_CONTRACT_V2){
+            transfer_tokens(FARM_CONTRACT_V2, asset(amount_to_send, quantity.symbol), tkcontract, FARM_DEPOSIT_MEMO(f.farm_name));
+        }  
+    }          
 
 } 
